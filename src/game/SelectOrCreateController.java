8f5d6a364f2258eb50918231c4904156047e79f8
swashyson <Gul_homer@hotmail.com>
2015-04-13 17:15:27 +0200

    Grunden till Spelet, Har lagt till alla scener fram till staden(staden är inte med) har gjort så att det funkar på en lokal mysql, har gjort så man kan skapa ett account och logga in, har lagt till lite funktionalitet lite över allt i alla scener. allting sparas korrekt och laddas in korrekt direkt från databasen. har gjort lite felhantering, men inte så mycket

---
+++ src/game/SelectOrCreateController.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mattias
+ */
+public class SelectOrCreateController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    private Button create;
+    @FXML
+    private Button choose;
+    @FXML
+    private Button back;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        try {
+            if (event.getSource().equals(create)) {
+
+                Node node = (Node) event.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("CharCreation.fxml"));
+                Parent root = loader.load();
+
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+            } else if (event.getSource().equals(choose)) {
+
+                Node node = (Node) event.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("ViewChar.fxml"));
+                Parent root = loader.load();
+
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+            } else if (event.getSource().equals(back)) {
+                Node node = (Node) event.getSource();
+                Stage stage = (Stage) node.getScene().getWindow();
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+                Parent root = loader.load();
+
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
