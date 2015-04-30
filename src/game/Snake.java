77408308e75754033ae4e1390203d28da766f504
moha0047 <imdifferent.hassler@gmail.com>
2015-04-30 19:17:49 +0200

    Har lag till nya klasser, i stort sett tomma. > Enemy > Scorpion > Bear > Snake > Spider > Wolf

---
+++ src/game/Snake.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author Mohini
+ */
+public class Snake extends Enemy{
+
+    public Snake(Hero hero) {
+        super(hero);
+    }
+
+    @Override
+    public void specialAttack1() {
+        
+    }
+
+    @Override
+    public void specialAttack2() {
+        
+    }
+    
+}
