1fef2e8bff6ae42a5982454263826614ebb47c0d
Jonni <Jonni@192.168.0.165>
2015-05-06 11:36:25 +0200

    testat olika men vill inte connecta till smtp.gmail.com

--- src/game/SendMail.java
+++ src/game/SendMail.java
@@ -13,45 +13,45 @@ import javax.mail.internet.*;
  */
 public class SendMail {
     private static final String SMTP_HOST_NAME = "smtp.gmail.com";
-         private static final String SMTP_PORT = "587";
+         private static final String SMTP_PORT = "465";
           private static final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
     public void Send(String email,String pword)throws Exception{
             // Get system properties
-       
-        boolean debug = true;
+        String adress = "LandOfThiralia@gmail.com"; 
+        String pass = "Thiralia";
         Properties props = System.getProperties();
 
         // Setup mail server
-        props.put("mail.smtp.user", "landofthiralia@gmail.com");
+        props.put("mail.smtp.user", adress);
         props.put("mail.smtp.host", SMTP_HOST_NAME);
         props.put("mail.smtp.auth", "true");
         props.put("mail.debug", "true");
+        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.port", SMTP_PORT);
-        //props.put("mail.smtp.starttls.enable","true");
         props.put("mail.smtp.socketFactory.port", SMTP_PORT);
         props.put("mail.smtp.socketFactory.class", SSL_FACTORY);
         props.put("mail.smtp.socketFactory.fallback", "false");
             Session session;
-            session = Session.getDefaultInstance(props, new Authenticator() {
+            session = Session.getInstance(props, new Authenticator() {
                 @Override
                 protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("LandOfThiralia@gmail.com", "Thiralia");
+                    return new PasswordAuthentication(adress, pass);
                 }}
                     
                     );
-            session.setDebug(debug);
+            session.setDebug(true);
         
         Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("LandOfThiralia@gmail.com"));
+        message.setFrom(new InternetAddress(adress));
         message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
         message.setSubject("Password Retrival");
         message.setText("You'r password are "+ pword);
         
-        Transport transport = session.getTransport("smtps");
-        transport.connect(SMTP_HOST_NAME, 465, "LandOfThiralia@gmail.com", "Thiralia");
-        transport.sendMessage(message, message.getAllRecipients());
-        transport.close();
-        //Transport.send(message);
+        Transport transport = session.getTransport("smtp");
+                transport.connect( "smtp.gmail.com",465,adress,pass); //host, 25, "myemailhere", "mypasshere");
+                message.saveChanges();
+                transport.sendMessage(message,message.getAllRecipients());
+    //Transport.send(message);
         }
 }
     
