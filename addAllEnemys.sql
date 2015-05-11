b6c9f530868d0da50f72d9a1cd478375c099417f
Jonni <Jonni@192.168.0.165>
2015-05-12 00:33:40 +0200

    Gjort ett Test enemy sql Script

---
+++ addAllEnemys.sql
@@ -0,0 +1,5 @@
+insert into game.enemy values(1,"Bear",10,1,2,1);
+insert into game.enemy values(2,"Scorpion",10,1,2,1);
+insert into game.enemy values(3,"Snake",10,1,2,1);
+insert into game.enemy values(4,"Spider",10,1,2,1);
+insert into game.enemy values(5,"Wolf",10,1,2,1)
